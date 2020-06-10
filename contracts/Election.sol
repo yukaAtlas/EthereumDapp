8daaf8cf562e445ed90ac9aa63fd74cb9c413954
yukaAtlas <yukako@atlasholdings.fund>
2020-06-09 20:21:49 -0700

    Added addCandidate() private function

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,12 +2,29 @@ pragma solidity 0.5.16;
 
 
 contract Election {
+    // Model candidate
+    struct Candidate {
+        uint256 id;
+        string name;
+        uint256 voteCount;
+    }
     // Store candidate
-    // Read candidate
-    string public candidate;
+    // Fetch candidate
+    mapping(uint256 => Candidate) public candidates;
+    // Store candidates Count
+    uint256 public candidatesCount;
 
     // Constructor
     constructor() public {
-        candidate = "Candidate 1";
+        // candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
+
+//Election.deployed().then(function(i){app=i})
